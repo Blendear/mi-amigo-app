b2d51cea289e13fd79931c1535e90c75590f7e0e
Blendear <tobiascibis@gmail.com>
2024-01-06 00:58:23 +0100

    style

--- src/features/add-events-with-needs/components/ImageAndInput.tsx
+++ src/features/add-events-with-needs/components/ImageAndInput.tsx
@@ -9,12 +9,22 @@ import {
 
 const ImageAndInput = ({ formDataRef, onChange }: EventFormSegmentProps) => {
   const [imagePath, setImagePath] = useState<string>(placeholderImagePath);
+  const [GIFPath, setGIFPath] = useState<string>(placeholderImagePath);
 
   return (
     <div className={styles["event-manager__form__img-n-input"]}>
       <div>
         <Image
           src={imagePath}
+          alt={"An image based on the given url image path"}
+          layout="fill"
+          objectFit="cover"
+          priority
+        />
+      </div>
+      <div>
+        <Image
+          src={GIFPath}
           alt={"An image based on the given url image path"}
           layout="fill"
           objectFit="cover"
@@ -29,6 +39,17 @@ const ImageAndInput = ({ formDataRef, onChange }: EventFormSegmentProps) => {
           onChange={(e) => {
             onChange("imagePath", e.target.value),
               setImagePath(generateImagePath(e.target.value));
+          }}
+        />
+      </label>
+      <label>
+        GIFPath
+        <input
+          type="text"
+          placeholder={formDataRef.current.GIFPath}
+          onChange={(e) => {
+            onChange("GIFPath", e.target.value),
+              setGIFPath(generateImagePath(e.target.value));
           }}
         />
       </label>
