3c3d209503c28cc39577efea3e2012135b6507b6
Blendear <tobiascibis@gmail.com>
2024-01-18 10:50:36 +0100

    feat

--- src/features/show-event-data/components/NeedsFullfilled.tsx
+++ src/features/show-event-data/components/NeedsFullfilled.tsx
@@ -1,33 +1,21 @@
 import styles from "@/styles/sass/styles-all.module.scss";
 import { NeedsFullfilledProps } from "../types/index";
 import ImageWithWrapper from "@/components/ImageWithWrapper";
-
-const needsImagePaths = {
-  isWant: "https://placehold.co/600x400",
-  isCreate: "https://placehold.co/600x500",
-  isAbsorb: "https://placehold.co/600x600",
-  isChill: "https://placehold.co/600x700",
-  isVent: "https://placehold.co/600x800",
-  isSquad: "https://placehold.co/600x900",
-  isActive: "https://placehold.co/600x100",
-  isWilderness: "https://placehold.co/600x300",
-  isFresh: "https://placehold.co/600x200",
-};
-
+import needsImagePaths from "../data/needsImagePaths";
 const NeedsFullfilled = ({ needs }: NeedsFullfilledProps) => {
   return (
     <ul className={styles["event-data-display__needs-fulfilled"]}>
       {Object.keys(needs).map((need, index) => (
         <li key={need}>
           <ImageWithWrapper
-            src={needsImagePaths[need]}
+            src={needsImagePaths[need].ifTrue || needsImagePaths[need]}
             width="100%"
             height="100%"
             wrapperClassName={needs[need] ? "" : styles["disabled"]}
           />
           {(index === 0 || index > 4) && (
             <ImageWithWrapper
-              src={needsImagePaths[need]}
+              src={needsImagePaths[need].ifFalse}
               width="100%"
               height="100%"
               wrapperClassName={needs[need] ? "" : styles["disabled"]}
---
+++ src/features/show-event-data/data/needsImagePaths.tsx
@@ -0,0 +1,28 @@
+const needsImagePaths = {
+  isWant: {
+    ifTrue: "https://placehold.co/600x400",
+    ifFalse: "https://placehold.co/300x200",
+  },
+  isCreate: "https://placehold.co/600x500",
+  isAbsorb: "https://placehold.co/600x600",
+  isChill: "https://placehold.co/600x700",
+  isVent: "https://placehold.co/600x800",
+  isSquad: {
+    ifTrue: "https://placehold.co/600x900",
+    ifFalse: "https://placehold.co/300x450",
+  },
+  isActive: {
+    ifTrue: "https://placehold.co/600x100",
+    ifFalse: "https://placehold.co/300x500",
+  },
+  isWilderness: {
+    ifTrue: "https://placehold.co/600x300",
+    ifFalse: "https://placehold.co/300x550",
+  },
+  isFresh: {
+    ifTrue: "https://placehold.co/600x200",
+    ifFalse: "https://placehold.co/300x600",
+  },
+};
+
+export default needsImagePaths;
