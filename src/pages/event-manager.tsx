fdfc3bd564d6c8186812232884b5a01bc02d9e6c
Blendear <tobiascibis@gmail.com>
2024-01-21 10:38:30 +0100

    chore

--- src/pages/event-manager.tsx
+++ src/pages/event-manager.tsx
@@ -1,9 +1,9 @@
 import styles from "@/styles/sass/styles-all.module.scss";
-import { ChestModalContent } from "../features/all-day-long-events-management";
+import ChestModalContent from "@/features/EventManager/Main/chest-of-daily-planned-and-unplanned-events/components/ChestModalContent";
 import { useState } from "react";
 import { DialogModal } from "..";
-import { ChestButton } from "../features/all-day-long-events-management";
-import Scheduler from "@/features/shedule-of-the-choosen-day/components/Scheduler";
+import ChestButton from "@/features/EventManager/Main/chest-of-daily-planned-and-unplanned-events/components/ChestButton";
+import Scheduler from "@/features/EventManager/Main/shedule-of-hourly-planned-events/components/Scheduler";
 
 const events = [
   {
