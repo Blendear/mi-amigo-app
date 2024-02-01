5512187e03176eab9d024422e62bd4b22fba6cf9
Blendear <tobiascibis@gmail.com>
2024-02-01 14:08:39 +0100

    feat

--- src/features/EventManager/Main/chest-of-daily-planned-and-unplanned-events/components/ChestModalContent.tsx
+++ src/features/EventManager/Main/chest-of-daily-planned-and-unplanned-events/components/ChestModalContent.tsx
@@ -38,6 +38,17 @@ const ChestModalContent = ({}: ChestModalContentProps) => {
   const closeModal = () => {
     setIsModalOpen(false);
   };
+  // herehere
+
+  // TODO: Create a special wrapper for an event "small display",
+  // which will have buttons to manage the event quickly and will have
+  // place for the animation on the right. We can use it in the
+  // "ChestModalContent", "LibraryPfTemplateEvents" (maybe one day in
+  // the schedule too, but its way to customized for now right there)
+
+  // TODO: Rework the "hardecodedEventsBecauseOfTheLackOfTime" to be
+  // an objetc with ALL placeholder data for the "EventManager" feature
+  // with separate events for the library, chest and schedule (later on)
 
   return (
     <div css={chestModalCss.container}>
@@ -47,6 +58,7 @@ const ChestModalContent = ({}: ChestModalContentProps) => {
             <h2 css={chestModalCss.titleBarWithContent}>For Today</h2>
           }
         >
+          {/* TODO: map over all chestWithAllDayLongEvents.necessary, and tender only those which "deadline" is for today or in the past */}
           ....
         </TitleBarWithTogglableContent>
       </section>
@@ -60,13 +72,13 @@ const ChestModalContent = ({}: ChestModalContentProps) => {
             return (
               <div
                 key={index}
-                style={{
+                css={{
                   marginTop: "2rem",
                 }}
               >
                 <button
                   onClick={() => openModal(index)}
-                  style={{
+                  css={{
                     width: "100%",
                     display: "grid",
                     justifyItems: "center",
@@ -74,7 +86,7 @@ const ChestModalContent = ({}: ChestModalContentProps) => {
                 >
                   <ImageWithWrapper
                     src={event.imagePath}
-                    width="50%"
+                    width="100%"
                     aspectRatio="2.63/1"
                   />
                   <div>{event.name}</div>
@@ -108,7 +120,7 @@ const ChestModalContent = ({}: ChestModalContentProps) => {
                 >
                   <ImageWithWrapper
                     src={event.imagePath}
-                    width="50%"
+                    width="100%"
                     aspectRatio="2.63/1"
                   />
                   <div>{event.name}</div>
